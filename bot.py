da79f4f8b83910b68224dfe1fd842b04650fda55
Osiroski <osiroskinoelski@gmail.com>
2021-04-29 15:24:12 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import logging
 from config import twitter_api
 import requests
 import pandas as pd
-port = int(os.environ.get(“PORT”, 5000))
+port = int(os.environ.get("PORT", 5000))
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
