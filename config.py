8d0097676a1030b0a39fe1ef45ed11bbbf922993
Osiroski <osiroskinoelski@gmail.com>
2021-05-14 18:42:09 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,8 @@
 import tweepy
 import logging
 from os import environ
-
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
-
 #Authenticate Twitter
 def twitter_api():
   auth=tweepy.OAuthHandler(environ['CONSUMER_KEY'],environ['CONSUMER_SECRET'])
